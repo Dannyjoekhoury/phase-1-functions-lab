a91d71a1ff66df538173b832d58ccca04f365087
Danny Joe Khoury <dannyjoekhoury@gmail.com>
2022-10-12 18:58:30 -0400

    commit tree 0837a77908c4b8bdddf72c64806f45127872f852

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const headquarters = 42;
+const block = 264;
+
+function distanceFromHqInBlocks(pickupLocation){
+        return Math.abs(pickupLocation - headquarters);
+}
+
+function distanceFromHqInFeet(pickupLocation){
+    return Math.abs(pickupLocation - headquarters)*block;
+}
+
+function distanceTravelledInFeet(pickupLocation, headquarters){
+    return Math.abs((pickupLocation-headquarters)*block);
+ }
+
+function calculatesFarePrice(pickupLocation, headquarters){
+    let distanceNew=distanceTravelledInFeet(pickupLocation,headquarters)
+    if(distanceNew <= 400){
+        return 0;
+    }
+    else if(distanceNew > 400 && distanceNew <= 2000){
+        return (distanceNew-400)*0.02;
+    }
+    else if(distanceNew > 2000 && distanceNew < 2500){
+        return 25;
+    }
+    else{
+        return "cannot travel that far";
+    }
+}
